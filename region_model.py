2da8fa7c1b1494789d4a26c9d5eda6bb11aaa83f
Timothy Roche <kc2vsa@gmail.com>
2021-02-28 22:33:14 -0500

    Got death working

--- region_model.py
+++ region_model.py
@@ -8,10 +8,10 @@ class region_model:
         self.S0 = self.N - self.I0 - self.R0
         # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
         self.beta, self.gamma = beta, gamma
-
         self.susceptible = [self.S0]
         self.infected = [self.I0]
         self.recovered = [self.R0]
+        self.dead = [0]
         self.time = [0]
         self.units = 1
         self.dt = 1
@@ -19,10 +19,16 @@ class region_model:
     def tickTime(self):
         t = self.units*self.dt
         self.time.append(t)
+        d_dea = 0
+        if len(self.dead) >= 14:
+            d_dea = self.infected[t-14]*0.018
         d_sus = -1*(self.beta*self.susceptible[t - 1]*self.infected[t - 1])/self.N
         d_inf = (self.beta*self.susceptible[t - 1]*self.infected[t - 1])/self.N - self.gamma*self.infected[t - 1] 
         d_rec = self.gamma*self.infected[t - 1]
+        
         self.susceptible.append(self.susceptible[t-1] + d_sus)
         self.infected.append(self.infected[t-1] + d_inf)
-        self.recovered.append(self.recovered[t-1] + d_rec)
+        self.recovered.append(self.recovered[t-1] + d_rec - d_dea)
+        self.dead.append(self.dead[t-1] + d_dea)
         self.units += 1
+        print(self.susceptible[-1] + self.infected[-1] + self.recovered[-1] + self.dead[-1])
