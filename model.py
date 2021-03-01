0ac3180da8aa11c25b5b928295b3ae5b58d89d84
Timothy Roche <kc2vsa@gmail.com>
2021-02-28 21:37:28 -0500

    Minor changes

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 class model:
-    def __init__(self):
+    def __init__(self, N=1000, I0=1, R0=0, beta=0.2, gamma=1./10):
         # Total population, N.
         self.N = 1000
         # Initial number of infected and recovered individuals, I0 and R0.
-        self.I0, self.R0 = 1, 0
+        self.I0, self.R0 = I0, R0
         # Everyone else, S0, is susceptible to infection initially.
         self.S0 = self.N - self.I0 - self.R0
         # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-        self.beta, self.gamma = 0.2, 1./10 
+        self.beta, self.gamma = beta, gamma
 
         self.susceptible = [self.S0]
         self.infected = [self.I0]
